9e8f64b5402c15bddb67dd67fa5b42a88696904c
Josh Shi <thehandsomepanther@gmail.com>
2017-05-16 02:08:31 -0500

    flesh out metainfo and info parsing

--- src/metainfo.rs
+++ src/metainfo.rs
@@ -19,7 +19,6 @@ fn get_field(map: &BTreeMap<ByteString, Bencode>, field: &str) -> Result<String,
         Some(contents) => Ok(contents.to_string()),
         None => panic!("Field not found!")
     }
-
 }
 
 pub enum Error {
@@ -31,6 +30,7 @@ pub enum Error {
 pub struct MetaInfo {
     pub announce: String,
     pub created_by: String,
+    pub info: Info,
 }
 
 impl FromBencode for MetaInfo {
@@ -39,18 +39,56 @@ impl FromBencode for MetaInfo {
     fn from_bencode(bn: &bencode::Bencode) -> Result<MetaInfo, Error> {
         match bn {
             &Bencode::Dict(ref m) => {
-                // let info = decode_field_as_bytes(m, "info");
+                let b = decode_field_as_bytes(m, "info");
+                let info: Bencode = bencode::from_vec(b).unwrap();
+                let decoded: Result<Info, Error> = FromBencode::from_bencode(&info);
+
                 let announce = get_field(m, "announce")?;
                 let created_by = get_field(m, "created by")?;
 
                 let metainfo = MetaInfo {
                     announce: announce,
                     created_by: created_by,
+                    info: decoded?
                 };
 
                 Ok(metainfo)
             }
             _ => panic!("Dict match error")
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Info {
+    pub piece_length: u32,
+    pub pieces: Vec<u8>,
+    pub num_pieces: u32,
+    pub name: String,
+    pub length: u64,
+}
+
+impl FromBencode for Info {
+    type Err = Error;
+
+    fn from_bencode(bencode: &bencode::Bencode) -> Result<Info, Error> {
+        match bencode {
+            &Bencode::Dict(ref m) => {
+                let pieces: Vec<u8> = decode_field_as_bytes(m, "pieces");
+                let num_pieces = pieces.len() as u32;
+                let length = get_field(m, "length")?;
+                let piece_length = get_field(m, "piece length")?;
+
+                let info = Info {
+                    piece_length: piece_length.parse::<u32>().unwrap(),
+                    pieces: pieces,
+                    num_pieces: num_pieces,
+                    name: get_field(m, "name")?,
+                    length: length.parse::<u64>().unwrap(),
+                };
+                Ok(info)
+            }
+            _ => Err(Error::DictMatchErr)
         }
     }
 }
